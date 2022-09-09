f0272c36bae3e4c07ed434177890a5c3ebdb5573
MaribelMOA <maribel.mendez@uabc.edu.mx>
2022-09-09 14:49:57 -0700

    Agregue Clase "WorkBreakDownStructure".

---
+++ WorkBreakDownStructure.cpp
@@ -0,0 +1,19 @@
+#include "WorkBreakDownStructure.h"
+
+int WorkBreakDownStructure::getName() {
+	// TODO - implement WorkBreakDownStructure::getName
+	return name;
+}
+
+void WorkBreakDownStructure::setName(int name) {
+	this->name = name;
+}
+
+int WorkBreakDownStructure::getTheTasks() {
+	// TODO - implement WorkBreakDownStructure::getTheTasks
+	return theTasks;
+}
+
+void WorkBreakDownStructure::setTheTasks(int theTasks) {
+	this->theTasks = theTasks;
+}
---
+++ WorkBreakDownStructure.h
@@ -0,0 +1,20 @@
+#ifndef WORKBREAKDOWNSTRUCTURE_H
+#define WORKBREAKDOWNSTRUCTURE_H
+
+class WorkBreakDownStructure {
+
+private:
+	int name;
+	int theTasks;
+
+public:
+	int getName();
+
+	void setName(int name);
+
+	int getTheTasks();
+
+	void setTheTasks(int theTasks);
+};
+
+#endif
